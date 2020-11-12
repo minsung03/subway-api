b412b2b3163a8549df30ed0169cdccc8698a9e10
park-bohee <parkbohee0705@gmail.com>
2020-11-12 22:07:46 +0900

    [ADD] no-cors 추가

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const axios = require('axios');
 const cheerio = require('cheerio');
+
+const app = express();
+
+app.use(cors());
 
 const crawling = async (path) => {
   const menus = [];
@@ -21,11 +25,11 @@ const crawling = async (path) => {
 
     const label = $(el).find('.label > .new');
     const kcal = $(el).find('.cal');
-    const view = $(el).find('btn_view');
+    const view = $(el).find('.btn_view');
 
-    menu['label'] = label ? label.text() : '';
-    menu['kcal'] = kcal ? kcal.text() : '';
-    menu['view_id'] = view ? view.data('id') : '';
+    menu['label'] = label && label.text() ? label.text() : '';
+    menu['kcal'] = kcal && kcal.text() ? kcal.text() : '';
+    menu['view_id'] = view && view.data('id') ? view.data('id') : '';
 
     menus.push(menu);
   });
@@ -34,30 +38,30 @@ const crawling = async (path) => {
 }
 
 app.get('/', (req, res) => {
-  res.send('Survey API');
+  res.send('subway API');
 });
 
-app.get('/survey/sandwich', async (req, res) => {
+app.get('/subway/sandwich', async (req, res) => {
   const sandwiches = await crawling('/sandwichList');
   res.json(sandwiches);
 });
 
-app.get('/survey/wrap', async (req, res) => {
+app.get('/subway/wrap', async (req, res) => {
   const wraps = await crawling('/wrapList');
   res.json(wraps);
 });
 
-app.get('/survey/salad', async (req, res) => {
+app.get('/subway/salad', async (req, res) => {
   const salads = await crawling('/saladList');
   res.json(salads);
 });
 
-app.get('/survey/side', async (req, res) => {
+app.get('/subway/side', async (req, res) => {
   const sides = await crawling('/sideDrink');
   res.json(sides);
 });
 
-app.get('/survey/catering', async (req, res) => {
+app.get('/subway/catering', async (req, res) => {
   const caterings = await crawling('/catering');
   res.json(caterings);
 });
